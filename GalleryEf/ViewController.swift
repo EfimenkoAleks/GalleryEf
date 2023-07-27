ab83543a983f9b9465f56cbcc079132de5af8206
gazservissana.zp@gmail.com <gazservissana.zp@gmail.com>
2023-07-27 18:48:00 +0300

    Initial Commit

---
+++ GalleryEf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryEf
+//
+//  Created by user on 27.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
